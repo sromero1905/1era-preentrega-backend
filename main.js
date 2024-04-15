df4556ff4f4a9709788d0022221bbf51967e88e2
sromero1905 <148464924+sromero1905@users.noreply.github.com>
2024-04-15 10:36:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+class ProductManager{
+constructor(){
+    this.products = []
+}
+addProduct(product) {
+    if (!Object.keys(product).every(field => ["title", "description", "price", "thumbnail", "code", "stock"].includes(field))) {
+        console.log("Error: Todos los campos son obligatorios");
+        return;
+    }
+
+    if (this.products.some(prod => prod.code === product.code)) {
+        console.log("Error: El código del producto ya existe");
+        return;
+    }
+
+    product.id = this.products.length + 1;
+    this.products.push(product);
+}
+getProducts() {
+    return this.products;
+}
+getProductById(id) {
+    const product = this.products.find(prod => prod.id === id);
+    if (product) {
+        return product;
+    } else {
+        console.log("Error: Producto no encontrado");
+    }
+}
+
+}
+const manager = new ProductManager();
+manager.addProduct({
+    title: "Producto 1",
+    description: "Descripción del producto 1",
+    price: 100.99,
+    thumbnail: "/foto1.avif",
+    code: "ABC123",
+    stock: 100
+});
+console.log(manager.getProducts());
+console.log(manager.getProductById(1));
